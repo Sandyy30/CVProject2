3cbbb6c9c29d8709a7fb28fecb817acefaa778de
Sandyy30 <98335389+Sandyy30@users.noreply.github.com>
2025-06-29 01:04:44 +0800

    Update u2net_train.py

--- u2net_train.py
+++ u2net_train.py
@@ -52,9 +52,12 @@ def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
 
 model_name = 'u2net' #'u2netp'
 
-data_dir = os.path.join(os.getcwd(), 'train1000' + os.sep)
+tra_data_dir = os.path.join(os.getcwd(), 'train1000' + os.sep)
 tra_image_dir = os.path.join('Images' + os.sep)
 tra_label_dir = os.path.join('Masks' + os.sep)
+val_data_dir = os.path.join(os.getcwd(), 'val200' + os.sep)
+val_image_dir = os.path.join('Images' + os.sep)
+val_label_dir = os.path.join('Masks' + os.sep)
 
 image_ext = '.jpg'
 label_ext = '.png'
@@ -68,9 +71,12 @@ batch_size_val = 1
 train_num = 0
 val_num = 0
 
-tra_img_name_list = glob.glob(data_dir + tra_image_dir + '*' + image_ext)
+tra_img_name_list = glob.glob(tra_data_dir + tra_image_dir + '*' + image_ext)
+val_img_name_list = glob.glob(val_data_dir + val_image_dir + '*' + image_ext)
 
 tra_lbl_name_list = []
+val_lbl_name_list = []
+
 for img_path in tra_img_name_list:
 	img_name = img_path.split(os.sep)[-1]
 
@@ -80,23 +86,48 @@ for img_path in tra_img_name_list:
 	for i in range(1,len(bbb)):
 		imidx = imidx + "." + bbb[i]
 
-	tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + label_ext)
+	tra_lbl_name_list.append(tra_data_dir + tra_label_dir + imidx + label_ext)
+
+for img_path in val_img_name_list:
+	img_name = img_path.split(os.sep)[-1]
+
+	aaa = img_name.split(".")
+	bbb = aaa[0:-1]
+	imidx = bbb[0]
+	for i in range(1,len(bbb)):
+		imidx = imidx + "." + bbb[i]
+
+	tra_lbl_name_list.append(val_data_dir + val_label_dir + imidx + label_ext)
+
 
 print("---")
 print("train images: ", len(tra_img_name_list))
 print("train labels: ", len(tra_lbl_name_list))
+print("validation images: ", len(val_img_name_list))
+print("validation labels: ", len(val_lbl_name_list))
 print("---")
 
 train_num = len(tra_img_name_list)
+val_num = len(val_img_name_list)
 
-salobj_dataset = SalObjDataset(
+train_dataset = SalObjDataset(
     img_name_list=tra_img_name_list,
     lbl_name_list=tra_lbl_name_list,
     transform=transforms.Compose([
         RescaleT(320),
         RandomCrop(288),
         ToTensorLab(flag=0)]))
-salobj_dataloader = DataLoader(salobj_dataset, batch_size=batch_size_train, shuffle=True, num_workers=1)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, num_workers=1)
+
+val_dataset = SalObjDataset(
+    img_name_list=val_img_name_list,
+    lbl_name_list=val_lbl_name_list,
+    transform=transforms.Compose([
+        RescaleT(320),
+        RandomCrop(288),
+        ToTensorLab(flag=0)]))
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size_train, shuffle=False, num_workers=1)
+
 
 # ------- 3. define model --------
 # define the net
@@ -120,14 +151,16 @@ running_tar_loss = 0.0
 ite_num4val = 0
 save_frq = 20 # save the model every 20 iterations
 
-epoch_losses = []
-epoch_target_losses = []
+train_epoch_losses = []
+train_epoch_target_losses = []
+val_epoch_losses = []
+val_epoch_target_losses = []
 
 for epoch in range(0, epoch_num):
     start_time = time.time()
     net.train()
 
-    for i, data in enumerate(salobj_dataloader):
+    for i, data in enumerate(train_dataloader):
         ite_num = ite_num + 1
         ite_num4val = ite_num4val + 1
 
@@ -159,27 +192,41 @@ for epoch in range(0, epoch_num):
 
         # del temporary outputs and loss
         del d0, d1, d2, d3, d4, d5, d6, loss2, loss
-
-        #print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " % (
-        #epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
-
-        # if ite_num % save_frq == 0:
-
-        #     torch.save(net.state_dict(), model_dir + model_name+"_bce_itr_%d_train_%3f_tar_%3f.pth" % (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
-        #     running_loss = 0.0
-        #     running_tar_loss = 0.0
-        #     net.train()  # resume train
-        #     ite_num4val = 0
+    avg_train_loss = running_loss / ite_num4val
+    avg_train_tar_loss = running_tar_loss / ite_num4val
+	
+    net.eval()
+    val_running_loss = 0.0
+    val_running_tar_loss = 0.0
+	
+    with torch.no_grad():
+        for data in val_dataloader:
+            inputs, labels = data['image'], data['label']
+            inputs = inputs.type(torch.FloatTensor)
+            labels = labels.type(torch.FloatTensor)
+	
+            if torch.cuda.is_available():
+                inputs_v, labels_v = inputs.cuda(), labels.cuda()
+            else:
+                inputs_v, labels_v = inputs, labels
+    
+            d0, d1, d2, d3, d4, d5, d6 = net(inputs_v)
+            val_loss2, val_loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v)
+	
+            val_running_loss += val_loss.data.item()
+            val_running_tar_loss += val_loss2.data.item()
 
     end_time = time.time()  # ⏱️ End timing
     elapsed_time = end_time - start_time
 
-    avg_loss = running_loss / ite_num4val
-    avg_tar_loss = running_tar_loss / ite_num4val
-    print(f"[Epoch {epoch + 1}/{epoch_num}] Average loss: {avg_loss:.6f}, Target loss: {avg_tar_loss:.6f}, Time Taken: {elapsed_time:.2f}s")
+    avg_val_loss = val_running_loss / len(val_dataloader)
+    avg_val_tar_loss = val_running_tar_loss / len(val_dataloader)
+    print(f"[Epoch {epoch + 1}/{epoch_num}] Train Loss: {avg_train_loss:.6f}, Train Target Loss: {avg_train_tar_loss:.6f}, Validation Loss: {avg_val_loss:.6f}, Validation Target Loss: {avg_val_tar_loss:.6f}, Time Taken: {elapsed_time:.2f}s")
 
-    epoch_losses.append(avg_loss)
-    epoch_target_losses.append(avg_tar_loss)
+    train_epoch_losses.append(avg_train_loss)
+    train_epoch_target_losses.append(avg_train_tar_loss)
+    val_epoch_losses.append(avg_val_loss)
+    val_epoch_target_losses.append(avg_val_tar_loss)
 
     # Reset epoch accumulators
     running_loss = 0.0
@@ -193,17 +240,14 @@ print(f"Final model saved to {final_model_path}")
 
 with open(os.path.join(model_dir, 'loss_log.csv'), 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['Epoch', 'Average Loss', 'Target Loss'])
-    for i in range(len(epoch_losses)):
-        writer.writerow([i+1, epoch_losses[i], epoch_target_losses[i]])
+    writer.writerow(['Epoch', 'Train Loss', 'Train Target Loss', 'Val Loss', 'Val Target Loss'])
+    for i in range(len(train_epoch_losses)):
+        writer.writerow([
+            i + 1,
+            train_epoch_losses[i],
+            train_epoch_target_losses[i],
+            val_epoch_losses[i],
+            val_epoch_target_losses[i]
+        ])
 
 print(f"Losses saved to {os.path.join(model_dir, 'loss_log.csv')}")
-
-plt.plot(epoch_losses, label='Total Loss')
-plt.plot(epoch_target_losses, label='Target Loss (d0)')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-plt.title('Training Loss Over Epochs')
-plt.savefig(os.path.join(model_dir, 'loss_plot.png'))
-plt.show()
